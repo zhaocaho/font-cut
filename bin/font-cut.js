d5e2b3c9719a8bcadc9660355362ce548816ce02
赵超 <243211921@qq.com>
2022-08-09 20:07:36 +0800

    feat 新增可执行文件

--- bin/font-cut.js
+++ bin/font-cut.js
@@ -1,5 +1,19 @@
-// const FontTransfer = require('../utils/font-transfer.js');
-// console.log(new FontTransfer());
+const program = require('commander');
+const FontTransfer = require('../utils/font-transfer');
 
-const a = require('../utils/font-transfer');
-console.log(a);
+program
+  .version(require('../package.json').version)
+  .usage('[options] <file or dir ...>')
+  .option('-s, --source <path>', 'character file path or dir')
+  .option('-f, --font <path>', 'origin font file path')
+  .option('-o, --output <filepath>', 'filepath to output font files')
+  .parse();
+
+const options = program.opts();
+
+console.log(options);
+
+const fontTransfer = new FontTransfer(options);
+fontTransfer.output();
+
+console.log(FontTransfer);
